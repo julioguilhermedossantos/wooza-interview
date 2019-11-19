8b87be70b49281ea69b1ca7679cb24c3fe6a7a92
Júlio Guilherme dos Santos <julioguilhermedossantos@yahoo.com.br>
2019-11-18 21:21:43 -0300

    Updates

---
+++ precache-manifest.94ae8074784093ed83f91da0635ae307.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac54c3cf5a2b060e65be0fee0cc35ee",
+    "url": "/wooza-interview/index.html"
+  },
+  {
+    "revision": "50e33c9dd258c7f8fae8",
+    "url": "/wooza-interview/static/css/main.ba0f8404.chunk.css"
+  },
+  {
+    "revision": "0f3e7f9340b7cad1f838",
+    "url": "/wooza-interview/static/js/2.c74a8f86.chunk.js"
+  },
+  {
+    "revision": "50e33c9dd258c7f8fae8",
+    "url": "/wooza-interview/static/js/main.545f398e.chunk.js"
+  },
+  {
+    "revision": "b4a9189f89d20f64a526",
+    "url": "/wooza-interview/static/js/runtime-main.15983317.js"
+  }
+]);
